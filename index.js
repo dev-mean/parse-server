00e32b95a97028a65ace6290fbdb39ac9ded7ea7
Jugal Kishor <jugal.ionic@gmail.com>
2016-05-30 22:39:26 +0530

    Signed-off-by: Jugal Kishor <jugal.ionic@gmail.com>

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // compatible API routes.
 //mongo --port 27017 --ssl --sslAllowInvalidCertificates --authenticationDatabase admin --username sammy --password
 //mongodb://parse:password@159.203.204.76:27017/popii_parse
-//parse-server --appId B88zTS61xWg64r0SfYjFAL1kq7x9LYDzNFNykKU1 --masterKey oTq8jsoCJiUuzonNVHNGzBP58CBBHrlECCSQu1dn
+//parse-server --appId Nw47gz5W9KvrZMxKCaFV6GGfwBw4kC4BkFz4EB7f --masterKey 6UzqOhby6pmAqyipt7xcgiNtlnDJkhUioB1lm6JW
 
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
@@ -12,9 +12,11 @@ var path = require('path');
 var api = new ParseServer({
   databaseURI: 'mongodb://localhost:27017/popii_parse',
   cloud:  __dirname + '/cloud/main.js',
-  appId: 'B88zTS61xWg64r0SfYjFAL1kq7x9LYDzNFNykKU1',
-  masterKey:'oTq8jsoCJiUuzonNVHNGzBP58CBBHrlECCSQu1dn', //Add your master key here. Keep it secret!
+  appId: 'Nw47gz5W9KvrZMxKCaFV6GGfwBw4kC4BkFz4EB7f',
+  masterKey:'6UzqOhby6pmAqyipt7xcgiNtlnDJkhUioB1lm6JW', //Add your master key here. Keep it secret!
   javascriptKey:'HZCSf3Kt6hxb9sENGuFEp17EAjOZuL7hpFc1PaCY',
+  restAPIKey:'akV3idRdPE3tzV8KBEkeL6Q2GDj5zPnswBlb9laO',
+  fileKey:'1f508bb2-6ecd-4791-a17b-6c78a3f0f832',
   serverURL: 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Products", "Shows"] // List of classes to support for query subscriptions
@@ -22,7 +24,7 @@ var api = new ParseServer({
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
-// javascriptKey, restAPIKey, dotNetKey, clientKey
+// javascriptKey,  dotNetKey, clientKey
 
 var app = express();
 var express = require('express');
